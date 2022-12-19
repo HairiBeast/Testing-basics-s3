621d85e7ce849cb2c345f1c44bcc0a33bf8de914
HairiBeast <shpetzla@gmail.com>
2022-12-19 13:50:21 +0000

    Added extra tests to assert a throw if either nothing is put in, or multiple arguments - added custom error message to narrow down any potential issues

--- code/basic-testing-frontend/src/math.test.js
+++ code/basic-testing-frontend/src/math.test.js
@@ -28,13 +28,46 @@ it("should yield NaN if at least one invalid nnumber is provided", () => {
 });
 
 it("Should yield a correct sum if an array of numeric string values is provided", () => {
+  //Arrange
   const numbers = ["1", "2"];
 
+  //Act
   const result = add(numbers);
 
+  //Assert
   const expectedResult = numbers.reduce(
     (prevValue, curValue) => +prevValue + +curValue,
     0
   );
   expect(result).toBe(expectedResult);
+});
+
+it("Should yield 0 as a sum if an empty array is provided", () => {
+  //Arrange
+  const numbers = [];
+
+  //Act
+  const result = add(numbers);
+
+  //Assert
+  expect(result).toBe(0);
+});
+
+it("Should throw an error if no value is passed into the function", () => {
+  const resultFn = () => {
+    add();
+  };
+
+  expect(resultFn).toThrow(/is not iterable/);
+});
+
+it("Should throw an error if provided with multiple arguments instead of handed an array", () => {
+  const num1 = 1;
+  const num2 = 2;
+
+  const resultFn = () => {
+    add(num1, num2);
+  };
+
+  expect(resultFn).toThrow(/is not iterable/);
 });
