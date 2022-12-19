731a646f9078255a7e57f7735be00ebd2c592ff1
HairiBeast <shpetzla@gmail.com>
2022-12-19 13:08:34 +0000

    added code to test authentication within function, reformatted add function to change strings to numbers and reflected this in tests

--- code/basic-testing-frontend/src/math.test.js
+++ code/basic-testing-frontend/src/math.test.js
@@ -15,3 +15,26 @@ it("should summarise all number values in an array", () => {
   //Assert
   expect(result).toBe(expectedResult);
 });
+
+it("should yield NaN if at least one invalid nnumber is provided", () => {
+  //Arrange
+  const inputs = ["invalid", 1];
+
+  //Act
+  const result = add(inputs);
+
+  //Assert
+  expect(result).toBeNaN();
+});
+
+it("Should yield a correct sum if an array of numeric string values is provided", () => {
+  const numbers = ["1", "2"];
+
+  const result = add(numbers);
+
+  const expectedResult = numbers.reduce(
+    (prevValue, curValue) => +prevValue + +curValue,
+    0
+  );
+  expect(result).toBe(expectedResult);
+});
